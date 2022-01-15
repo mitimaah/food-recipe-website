5390e78bff319f7ef606631d3806aacd4f71d775
mitimaah <mitimaah@gmail.com>
2022-01-15 15:07:37 +0100

    navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'Categories',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Search',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
