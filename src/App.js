3c2da1689a89e270262b3844bb81fbb04f767d6d
mitimaah <mitimaah@gmail.com>
2022-01-26 00:20:46 +0100

    navbar updete changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Navbar from "./components/Navbar/Navbar";
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar2/Navbar";
+import "./App.css";
 
 function App() {
   return (
---
+++ src/components/Navbar/Navbar2/Navbar.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { menuItems } from "../MenuItems";
+import { Button } from "../../Button";
+import "./Navbar.css";
+
+class Navbar extends Component {
+  state = { clicked: false };
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  render() {
+    return (
+      <nav className="navbar-items">
+        <h1 className="navbar-logo">
+          Eat IT<i className="fab fa-react"></i>
+        </h1>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {menuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <Button className="button">Sign Up</Button>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
