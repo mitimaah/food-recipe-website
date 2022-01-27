989fc7f7d7775a9707cd0de2f90e29ef3ad93288
mitimaah <mitimaah@gmail.com>
2022-01-27 22:42:18 +0100

    slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar2/Navbar";
 import "./App.css";
+import Slider from './components/Slider/Slider';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Slider />
+
+      {/* <Recipies /> */}
+      {/* <Footer /> */}
+
     </div>
   );
 }
